926adf33ca940ec2725c1eb623e4520053fe4496
arthur <arthurdedsec@gmail.com>
2025-09-27 09:41:14 -0300

    Feat: iconuser finalizada

---
+++ src/components/IconUser.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function IconUser() {
+    return (
+        <div className="">
+            <Image width={100} height={10} alt="iconuser" src="/iconuser.png" className="rounded-full" />
+        </div>
+    )
+}
